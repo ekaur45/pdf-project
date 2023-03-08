0ed3e19b1754f6674ab1c36314520ba5cc4c9e80
waqas.ahmad <waqas.ahmad@wisemani.com>
2023-03-08 14:51:02 +0500

    done

--- pdf-fe/src/app/account/login/login.component.ts
+++ pdf-fe/src/app/account/login/login.component.ts
@@ -12,6 +12,12 @@ export class LoginComponent implements OnInit {
   constructor(private api:ApiService,private router:Router) { }
 
   ngOnInit(): void {
+    let s = document.createElement("script");
+    s.src = "assets/js/plugins.min.js";
+    document.body.appendChild(s);
+    let s1 = document.createElement("script");
+    s1.src = "assets/js/script.min.js";
+    document.body.appendChild(s1);
   }
   onFormSubmit(e:any){
     this.api.post('account/login',e.value).subscribe((x:any)=>{
