3302e41fb5d722f047e568e74c5a90146b5a6626
= <=>
2023-03-23 01:23:58 +0500

    roomType

--- pdf-be/models/misc/misc-db.model.js
+++ pdf-be/models/misc/misc-db.model.js
@@ -20,10 +20,10 @@ Misc.deleteHotel = async id => {
     return await mysqlExecute('delete from hotel where id=?', [id], false);
 }
 Misc.updateRoomType = async data => {
-    return await mysqlExecute('update roomTypes set `display`=?,description=? where id=?', [data.name,data.description, data.id], false);
+    return await mysqlExecute('update roomtypes set `display`=?,description=? where id=?', [data.name,data.description, data.id], false);
 }
 Misc.deleteRoomType = async id => {
-    return await mysqlExecute('delete from roomTypes where id=?', [id], false);
+    return await mysqlExecute('delete from roomtypes where id=?', [id], false);
 }
 Misc.updateDestination = async data => {
     return await mysqlExecute('update destinations set `display`=? where id=?', [data.name, data.id], false);
