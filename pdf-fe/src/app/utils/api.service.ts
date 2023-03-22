9c48011483da93ae9ba1e66e2cd2c59a747e83b8
= <=>
2023-03-23 00:15:36 +0500

    Invalid token

--- pdf-fe/src/app/utils/api.service.ts
+++ pdf-fe/src/app/utils/api.service.ts
@@ -1,17 +1,27 @@
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
 import { ApiResponse } from '../shared/models/response.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   baseUrl = environment.api;
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private router:Router) { }
   get(url:string):Observable<ApiResponse>{
-    return this.http.get<ApiResponse>(this.baseUrl+url);
+    return this.http.get<ApiResponse>(this.baseUrl+url).pipe(map((x:any)=>{
+      if(x.status == 401){
+        return this.router.navigate(['/account']);
+      }
+      return x;
+    }),(e:any)=>{
+      
+      return e;
+    });
   }
   post(url:string,data:any):Observable<ApiResponse>{
     return this.http.post<ApiResponse>(this.baseUrl+url,data);
